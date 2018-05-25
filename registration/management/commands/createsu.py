8978c0fe20a5c5afa6d17730f723472c6d3640a3
njnisarg <NJnisarg@gmail.com>
2018-05-26 00:58:33 +0530

    Admin creation issues battle 1

--- registration/management/commands/createsu.py
+++ registration/management/commands/createsu.py
@@ -6,4 +6,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("ProfMatchAdmin", "ProfMatchAdmin@gmail.com", "djangoadminpassword")
+            User.objects.create_superuser("profmatchadmin", "profmatchadmin@gmail.com", "djangoadminpassword")
