de83d10940dfe06083c0e77700e82fe41311315e
njnisarg <NJnisarg@gmail.com>
2018-05-26 00:26:44 +0530

    Added AWS RDS config in settings file

--- IEEEProfMatch/settings.py
+++ IEEEProfMatch/settings.py
@@ -84,19 +84,28 @@ WSGI_APPLICATION = 'IEEEProfMatch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-      # The default sqlite database
-      #   'ENGINE': 'django.db.backends.sqlite3',
-      #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-      # The Production database config
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ProfMatch',
-        'USER': '',
-        'PASSWORD': '',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'ProfMatch',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
