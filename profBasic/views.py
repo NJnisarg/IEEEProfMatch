8bbc6e216b2da5df902c482986f087ebe1611528
njnisarg <NJnisarg@gmail.com>
2018-07-29 12:36:07 +0530

    Added parser classes

--- profBasic/views.py
+++ profBasic/views.py
@@ -2,6 +2,7 @@ from profBasic.models import profBasic
 from profBasic.serializers import profBasicSerializer, ImageSerializer
 from django.http import Http404
 from rest_framework import generics
+from rest_framework.parsers import MultiPartParser, FileUploadParser, FormParser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -38,8 +39,10 @@ class profBasicCRUD(APIView):
 class ImageList(generics.ListCreateAPIView):
     queryset = profBasic.objects.all()
     serializer_class = ImageSerializer
+    parser_classes = (MultiPartParser, FormParser, FileUploadParser, )
 
 
 class ImageDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = profBasic.objects.all()
     serializer_class = ImageSerializer
+    parser_classes = (MultiPartParser, FormParser, FileUploadParser,)
