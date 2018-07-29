a03c9d645506b9cf23448b29a0f93735b7cfac3f
njnisarg <NJnisarg@gmail.com>
2018-07-29 12:25:50 +0530

    Changes to bucket name

--- profBasic/urls.py
+++ profBasic/urls.py
@@ -4,5 +4,5 @@ from profBasic import views
 urlpatterns = [
     path('crud/<pk>/', views.profBasicCRUD.as_view()),
     path('profImage/list/', views.ImageList.as_view()),
-    path('profImage/crud/', views.ImageDetail.as_view()),
+    path('profImage/crud/<pk>/', views.ImageDetail.as_view()),
 ]
